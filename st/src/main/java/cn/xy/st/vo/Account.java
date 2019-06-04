683e20fb20463a3165a344eac1feac5c4656dd86
xuyao <xuyao@Haier-PC>
2019-06-04 23:36:04 +0800

    Signed-off-by: xuyao <xuyao@Haier-PC>

---
+++ st/src/main/java/cn/xy/st/vo/Account.java
@@ -0,0 +1,28 @@
+package cn.xy.st.vo;
+
+public class Account {
+
+	double balance;//总资产
+	int amount;//股票手数
+	double left;//剩余资产
+	
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public double getLeft() {
+		return left;
+	}
+	public void setLeft(double left) {
+		this.left = left;
+	}
+	
+}
