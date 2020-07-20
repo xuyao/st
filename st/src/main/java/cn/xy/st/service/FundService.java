e19d975d90fba75d4b768988d58e8cfaa1397347
xuyao <58394322@qq.com>
2020-07-20 16:33:30 +0800

    Signed-off-by: xuyao <58394322@qq.com>

--- st/src/main/java/cn/xy/st/service/FundService.java
+++ st/src/main/java/cn/xy/st/service/FundService.java
@@ -22,6 +22,7 @@ public class FundService {
 	
 	HttpUtil http = new HttpUtil();
 	String ft = ConstsUtil.getValue("ft");
+	String num = ConstsUtil.getValue("num");
 	String[] args={"zzf","1yzf","3yzf","6yzf","1nzf","2nzf","3nzf","jnzf","lszf"};
 	
 	String[][] year={{"2019-01-01","2020-01-01"},{"2018-01-01","2019-01-01"},{"2017-01-01","2018-01-01"},{"2016-01-01","2017-01-01"}};
@@ -46,7 +47,7 @@ public class FundService {
 		for(String arg : args){
 			List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
 			String url = "http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft="+ft+"&rs=&gs=0&sc="+arg+"&st=desc&sd="+date+
-					"&ed="+date+"&qdii=&tabSubtype=,,,,,&pi=1&pn=2000&dx=1&v=0."+NumberUtil.getRandomNum(17);
+					"&ed="+date+"&qdii=&tabSubtype=,,,,,&pi=1&pn="+num+"&dx=1&v=0."+NumberUtil.getRandomNum(17);
 			try {
 				ArrayList<String> listfund = new ArrayList<String>();
 				String result = http.sendGet(url, nameValuePairList);
@@ -80,7 +81,7 @@ public class FundService {
 		for(String[] ys : year){
 			List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
 			String url = "http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft="+ft+"&rs=&gs=0&sc=qjzf&st=desc&sd="+ys[0]+
-					"&ed="+ys[1]+"&qdii=&tabSubtype=,,,,,&pi=1&pn=2000&dx=1&v=0."+NumberUtil.getRandomNum(17);
+					"&ed="+ys[1]+"&qdii=&tabSubtype=,,,,,&pi=1&pn="+num+"&dx=1&v=0."+NumberUtil.getRandomNum(17);
 			try {
 				ArrayList<String> listfund = new ArrayList<String>();
 				String result = http.sendGet(url, nameValuePairList);
