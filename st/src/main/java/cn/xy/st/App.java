65c9f2d35530e94088b1514d590dcbb59754b28a
xuyao <58394322@qq.com>
2020-07-20 16:25:12 +0800

    Signed-off-by: xuyao <58394322@qq.com>

--- st/src/main/java/cn/xy/st/App.java
+++ st/src/main/java/cn/xy/st/App.java
@@ -3,24 +3,16 @@ package cn.xy.st;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import cn.xy.st.service.ShareService;
-import cn.xy.st.service.ZqService;
+import cn.xy.st.service.FundService;
 import cn.xy.st.util.ConstsUtil;
 
 public class App {
     public static void main( String[] args ) {
-		String service = ConstsUtil.getValue("service");
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContent.xml");
 		
-		if("share".equals(service)){
-			ShareService fs = (ShareService)context.getBean("shareService");
-			fs.run();
-		}
+		FundService fs = (FundService)context.getBean("fundService");
+		fs.run();
 
-		if("zq".equals(service)){
-			ZqService zs = (ZqService)context.getBean("zqService");
-			zs.run();
-		}
     }
     
 }
--- st/src/main/java/cn/xy/st/service/FundService.java
+++ st/src/main/java/cn/xy/st/service/FundService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.apache.http.NameValuePair;
 import org.springframework.stereotype.Service;
 
+import cn.xy.st.util.ConstsUtil;
 import cn.xy.st.util.DateUtil;
 import cn.xy.st.util.HttpUtil;
 import cn.xy.st.util.NumberUtil;
@@ -14,26 +15,24 @@ import cn.xy.st.util.StrUtils;
 
 /**
  * @author xuyao
- *	bond fund
+ *	share fund
  */
 @Service
-public class ZqService {
+public class FundService {
 	
 	HttpUtil http = new HttpUtil();
-//	String[] args={"zzf"};
+	String ft = ConstsUtil.getValue("ft");
 	String[] args={"zzf","1yzf","3yzf","6yzf","1nzf","2nzf","3nzf","jnzf","lszf"};
 	
-	String[][] year={{"2019-01-01","2020-01-01"},{"2018-01-01","2019-01-01"},{"2017-01-01","2018-01-01"},{"2016-01-01","2017-01-01"},
-			{"2015-01-01","2016-01-01"},{"2014-01-01","2015-01-01"}};
-	
-//	String[][] year={{"2019-01-01","2020-01-01"},{"2018-01-01","2019-01-01"},{"2017-01-01","2018-01-01"}};
-	
-	Integer num = 800;
+	String[][] year={{"2019-01-01","2020-01-01"},{"2018-01-01","2019-01-01"},{"2017-01-01","2018-01-01"},{"2016-01-01","2017-01-01"}};
+//	String[][] year={{"2019-01-01","2020-01-01"},{"2018-01-01","2019-01-01"},{"2017-01-01","2018-01-01"},{"2016-01-01","2017-01-01"},{"2015-01-01","2016-01-01"}
+//	,{"2014-01-01","2015-01-01"},{"2013-01-01","2014-01-01"}};
 	
 	public void run(){
 		List<String> list1 = guding();
 		List<String> list2 = year();
 		list1.retainAll(list2);
+		
 		System.out.println("==================分析基金如下");
 		for(String s : list1){
 			System.out.println(s);
@@ -46,21 +45,16 @@ public class ZqService {
 		String date = DateUtil.format(new Date(), DateUtil.STR_DATE_PATTERN);
 		for(String arg : args){
 			List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
-			String url = "http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=zq&rs=&gs=0&sc="+arg+"&st=desc&sd="+date+"&ed="+date+
-					"&qdii=&tabSubtype=,,,,,&pi=1&pn="+num+"&dx=1&v=0."+NumberUtil.getRandomNum(17);
+			String url = "http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft="+ft+"&rs=&gs=0&sc="+arg+"&st=desc&sd="+date+
+					"&ed="+date+"&qdii=&tabSubtype=,,,,,&pi=1&pn=2000&dx=1&v=0."+NumberUtil.getRandomNum(17);
 			try {
 				ArrayList<String> listfund = new ArrayList<String>();
 				String result = http.sendGet(url, nameValuePairList);
-				String all = StrUtils.subString(result, "allRecords:", ",pageIndex");
-				Integer top = Integer.parseInt(all)/3;
 				result = StrUtils.subString(result, "[\"", "\"]");
 				String[] line = result.split("\",\"");
-				for(String s : line){
-					if(top==0)
-						break;
+				for(String s : line ){
 					String[] c = s.split(",");
 					listfund.add(c[0]);
-					top--;
 				}
 				
 				if(list.size() == 0)
@@ -85,21 +79,16 @@ public class ZqService {
 		List<String> list = new ArrayList<String>();
 		for(String[] ys : year){
 			List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
-			String url = "http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=zq&rs=&gs=0&sc=qjzf&st=desc&sd="+ys[0]+
-					"&ed="+ys[1]+"&&qdii=&tabSubtype=,,,,,&pi=1&pn="+num+"&dx=1&v=0."+NumberUtil.getRandomNum(17);
+			String url = "http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=all&rs=&gs=0&sc=qjzf&st=desc&sd="+ys[0]+
+					"&ed="+ys[1]+"&qdii=&tabSubtype=,,,,,&pi=1&pn=2000&dx=1&v=0."+NumberUtil.getRandomNum(17);
 			try {
 				ArrayList<String> listfund = new ArrayList<String>();
 				String result = http.sendGet(url, nameValuePairList);
-				String all = StrUtils.subString(result, "allRecords:", ",pageIndex");
-				Integer top = Integer.parseInt(all)/3;
 				result = StrUtils.subString(result, "[\"", "\"]");
 				String[] line = result.split("\",\"");
 				for(String s : line ){
-					if(top==0)
-						break;
 					String[] c = s.split(",");
 					listfund.add(c[0]);
-					top--;
 				}
 				
 				if(list.size() == 0)
